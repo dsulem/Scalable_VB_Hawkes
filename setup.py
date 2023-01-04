c1429a20cb21ade49b7af0725270ce96d4ecd826
dsulem <deborah.sulem@spc.ox.ac.uk>
2023-01-04 14:55:18 -0600

    Adaptive VB algorithms and Example 1

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Scalable_VB_Hawkes',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='sulem',
+    author_email='',
+    description=''
+)
